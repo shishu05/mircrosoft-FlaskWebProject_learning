41554f9dcb0227b24888f227949747ca62cae32d
shishu pal singh <shishu05@hotmail.com>
2019-08-19 10:50:12 +0530

    add the html page in template folder

--- FlaskWebProject_learning/HelloFlask/views.py
+++ FlaskWebProject_learning/HelloFlask/views.py
@@ -1,8 +1,20 @@
-from flask import Flask
+from datetime import datetime
+
+from flask import render_template
 from HelloFlask import app
 
 @app.route('/')
 @app.route('/home')
 
 def home():
-    return "Hello Shishu"
+    now =datetime.now()
+    formatted_now = now.strftime ("%A, %d %B, %Y at %X")
+
+    #html_content="<html><head><title>Hello Shishu this Python code</title></head><body>"
+    #html_content +="<strong> Hello Flask!</strong> on " + formatted_now
+    #html_content += "</body></html>"
+    return render_template("index.html", 
+                           title="Hello Flask",
+                           message = "Hello, Shishu",
+                           content =" on " + formatted_now)
+#"Hello Shishu"
